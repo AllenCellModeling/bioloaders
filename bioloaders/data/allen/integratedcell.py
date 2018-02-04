fba9d0ce1db817323b7f9536f0f4660ea7b02082
Gregory Johnson <gregj@alleninstitute.org>
2018-02-04 12:49:15 -0800

    added skeleton IC object

---
+++ bioloaders/data/allen/integratedcell.py
@@ -0,0 +1,38 @@
+from bioloaders import Dataset
+
+class ic2d(Dataset):
+    
+    urls = [
+        'insert those AWS paths here',
+    ]
+    
+    def __init__(self, train=True, download=False):
+        pass
+    
+    def __getitem__(self, index):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def download(self):
+        pass
+
+    
+class ic3d(Dataset):
+
+    urls = [
+        'insert those AWS paths here',
+    ]
+
+    def __init__(self, train=True, download=False):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
+    
+    def download(self):
+        pass
